bd50a41b96468aeca19b092278ade62fbdc1893e
i_serdaroglu@thy.com <okanserdaroglu2@gmail.com>
2019-11-21 10:40:31 +0300

    splash added

---
+++ app/src/main/java/com/okanserdaroglu/searchablelistapp/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.okanserdaroglu.searchablelistapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        object : CountDownTimer(3000,1000) {
+            override fun onFinish() {
+
+                var intent = Intent(this@SplashActivity,MainActivity::class.java)
+                startActivity(intent)
+
+            }
+
+            override fun onTick(p0: Long) {}
+
+        }.start()
+
+    }
+}
